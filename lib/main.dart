4536df0f01651832b6b05f655e24eb1f9cc8524c
Louka Fauvel <louka.fauvel@gmail.com>
2022-04-04 12:27:57 +0200

    feat : page liste des groupes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:multi_timer/pages/groups.dart';
 
 void main() {
   runApp(const MyApp());
@@ -106,7 +107,12 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const groups(title: "/test"))
+          );//_incrementCounter,
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
