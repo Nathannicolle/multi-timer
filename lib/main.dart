03376cb9d341954f39a301a4c2ca96d97f6f7041
Nathannicolle <nathan-nicolle@orange.fr>
2022-04-06 09:41:36 +0200

    adding logo to GFAppbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:multi_timer/composant/AddTimer.dart';
 import 'package:multi_timer/pages/groups.dart';
@@ -53,105 +51,16 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: GFAppBar(
         leading: GFAvatar(
-            backgroundImage:AssetImage("assets/img/profile.png"),
+            backgroundImage:AssetImage("assets/img/logo_mini.png"),
             shape: GFAvatarShape.standard
         ),
         title: Text("Multi-timer app"),
         actions: <Widget>[],
       ),
-      body: Center(
-          child: CircularCountDownTimer(
-        // Countdown duration in Seconds.
-        duration: _duration,
-        // Countdown initial elapsed Duration in Seconds.
-        initialDuration: 0,
-        // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
-        controller: _controller,
-        // Width of the Countdown Widget.
-        width: MediaQuery.of(context).size.width / 2,
-        // Height of the Countdown Widget.
-        height: MediaQuery.of(context).size.height / 2,
-        // Ring Color for Countdown Widget.
-        ringColor: Colors.grey[300]!,
-        // Ring Gradient for Countdown Widget.
-        ringGradient: null,
-        // Filling Color for Countdown Widget.
-        fillColor: Colors.purpleAccent[100]!,
-        // Filling Gradient for Countdown Widget.
-        fillGradient: null,
-        // Background Color for Countdown Widget.
-        backgroundColor: Colors.blue[800],
-        // Background Gradient for Countdown Widget.
-        backgroundGradient: null,
+        body: Column(
+          children: <Widget>[CircularCountDownTimer(width: 100, height: 100, duration: 60, fillColor: Colors.red, ringColor: Colors.blue,autoStart: true),],
 
-        // Border Thickness of the Countdown Ring.
-        strokeWidth: 20.0,
-        // Begin and end contours with a flat edge and no extension.
-        strokeCap: StrokeCap.round,
-        // Text Style for Countdown Text.
-        textStyle: const TextStyle(
-          fontSize: 33.0,
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
-        // Format for the Countdown Text.
-        textFormat: CountdownTextFormat.S,
-        // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
-        isReverse: false,
-        // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
-        isReverseAnimation: false,
-        // Handles visibility of the Countdown Text.
-        isTimerTextShown: true,
-        // Handles the timer start.
-        autoStart: false,
-        // This Callback will execute when the Countdown Starts.
-        onStart: () {
-          // Here, do whatever you want
-          debugPrint('Countdown Started');
-        },
-        // This Callback will execute when the Countdown Ends.
-        onComplete: () {
-          // Here, do whatever you want
-          debugPrint('Countdown Ended');
-        },
-      )),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            width: 30,
-          ),
-          _button(title: "Start", onPressed: () => _controller.start()),
-          const SizedBox(
-            width: 10,
-          ),
-          _button(title: "Pause", onPressed: () => _controller.pause()),
-          const SizedBox(
-            width: 10,
-          ),
-          _button(title: "Resume", onPressed: () => _controller.resume()),
-          const SizedBox(
-            width: 10,
-          ),
-          _button(
-              title: "Restart",
-              onPressed: () => _controller.restart(duration: _duration))
-        ],
-      ),
+        )
     );
-  }
-
-Widget _button({required String title, VoidCallback? onPressed}) {
-    return Expanded(
-        child: ElevatedButton(
-      child: Text(
-        title,
-        style: const TextStyle(color: Colors.white),
-      ),
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(Colors.black),
-      ),
-      onPressed: onPressed,
-    ));
   }
 }
