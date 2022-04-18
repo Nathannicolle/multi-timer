9f5a09242f891daed7743a37e912e8f84d53b499
Nathannicolle <nathan-nicolle@orange.fr>
2022-04-18 23:56:51 +0200

    edited background and adding bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:getwidget/components/appbar/gf_appbar.dart';
 import 'package:getwidget/components/avatar/gf_avatar.dart';
+import 'package:getwidget/components/bottom_sheet/gf_bottom_sheet.dart';
+import 'package:getwidget/components/button/gf_icon_button.dart';
 import 'package:getwidget/shape/gf_avatar_shape.dart';
 
 void main() => runApp(const MyApp());
@@ -15,9 +17,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Circular Countdown Timer Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
       ),
       home: const MyHomePage(title: 'Circular Countdown Timer'),
     );
@@ -40,131 +40,129 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: GFAppBar(
-        leading: GFAvatar(
-        backgroundImage:AssetImage("assets/img/logo_mini.png"),
-       shape: GFAvatarShape.standard
-    ),
-    title: Text("Multi-timer app"),
-    actions: <Widget>[],
+        backgroundColor: Color.fromRGBO(18, 18, 18, 1),
+        body: Center(
+            child: CircularCountDownTimer(
+          // Countdown duration in Seconds.
+          duration: _duration,
 
-    ),
+          // Countdown initial elapsed Duration in Seconds.
+          initialDuration: 0,
 
-      body: Center(
-          child: CircularCountDownTimer(
-            // Countdown duration in Seconds.
-            duration: _duration,
+          // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
+          controller: _controller,
 
-            // Countdown initial elapsed Duration in Seconds.
-            initialDuration: 0,
+          // Width of the Countdown Widget.
+          width: MediaQuery.of(context).size.width / 2,
 
-            // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
-            controller: _controller,
+          // Height of the Countdown Widget.
+          height: MediaQuery.of(context).size.height / 2,
 
-            // Width of the Countdown Widget.
-            width: MediaQuery.of(context).size.width / 2,
+          // Ring Color for Countdown Widget.
+          ringColor: Colors.grey[300]!,
 
-            // Height of the Countdown Widget.
-            height: MediaQuery.of(context).size.height / 2,
+          // Ring Gradient for Countdown Widget.
+          ringGradient: null,
 
-            // Ring Color for Countdown Widget.
-            ringColor: Colors.grey[300]!,
+          // Filling Color for Countdown Widget.
+          fillColor: Colors.red[500]!,
 
-            // Ring Gradient for Countdown Widget.
-            ringGradient: null,
+          // Filling Gradient for Countdown Widget.
+          fillGradient: null,
 
-            // Filling Color for Countdown Widget.
-            fillColor: Colors.red[500]!,
+          // Background Color for Countdown Widget.
+          backgroundColor: Colors.blue[500],
 
-            // Filling Gradient for Countdown Widget.
-            fillGradient: null,
+          // Background Gradient for Countdown Widget.
+          backgroundGradient: null,
 
-            // Background Color for Countdown Widget.
-            backgroundColor: Colors.blue[500],
+          // Border Thickness of the Countdown Ring.
+          strokeWidth: 20.0,
 
-            // Background Gradient for Countdown Widget.
-            backgroundGradient: null,
+          // Begin and end contours with a flat edge and no extension.
+          strokeCap: StrokeCap.round,
 
-            // Border Thickness of the Countdown Ring.
-            strokeWidth: 20.0,
+          // Text Style for Countdown Text.
+          textStyle: const TextStyle(
+            fontSize: 33.0,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
 
-            // Begin and end contours with a flat edge and no extension.
-            strokeCap: StrokeCap.round,
+          // Format for the Countdown Text.
+          textFormat: CountdownTextFormat.S,
 
-            // Text Style for Countdown Text.
-            textStyle: const TextStyle(
-              fontSize: 33.0,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+          // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
+          isReverse: false,
+
+          // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
+          isReverseAnimation: false,
+
+          // Handles visibility of the Countdown Text.
+          isTimerTextShown: true,
+
+          // Handles the timer start.
+          autoStart: false,
+
+          // This Callback will execute when the Countdown Starts.
+          onStart: () {
+            // Here, do whatever you want
+            debugPrint('Countdown Started');
+          },
+
+          // This Callback will execute when the Countdown Ends.
+          onComplete: () {
+            // Here, do whatever you want
+            debugPrint('Countdown Ended');
+          },
+        )),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              width: 30,
             ),
-
-            // Format for the Countdown Text.
-            textFormat: CountdownTextFormat.S,
-
-            // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
-            isReverse: false,
-
-            // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
-            isReverseAnimation: false,
-
-            // Handles visibility of the Countdown Text.
-            isTimerTextShown: true,
-
-            // Handles the timer start.
-            autoStart: false,
-
-            // This Callback will execute when the Countdown Starts.
-            onStart: () {
-              // Here, do whatever you want
-              debugPrint('Countdown Started');
-            },
-
-            // This Callback will execute when the Countdown Ends.
-            onComplete: () {
-              // Here, do whatever you want
-              debugPrint('Countdown Ended');
-            },
-          )),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            width: 30,
-          ),
-          _button(title: "Start", onPressed: () => _controller.start()),
-          const SizedBox(
-            width: 10,
-          ),
-          _button(title: "Pause", onPressed: () => _controller.pause()),
-          const SizedBox(
-            width: 10,
-          ),
-          _button(title: "Resume", onPressed: () => _controller.resume()),
-          const SizedBox(
-            width: 10,
-          ),
-          _button(
-              title: "Restart",
-              onPressed: () => _controller.restart(duration: _duration))
-        ],)
-    );
+            _button(title: "Start", onPressed: () => _controller.start()),
+            const SizedBox(
+              width: 10,
+            ),
+            _button(title: "Pause", onPressed: () => _controller.pause()),
+            const SizedBox(
+              width: 10,
+            ),
+            _button(title: "Resume", onPressed: () => _controller.resume()),
+            const SizedBox(
+              width: 10,
+            ),
+            _button(
+                title: "Restart",
+                onPressed: () => _controller.restart(duration: _duration))
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Color.fromRGBO(57, 57, 57, 1),
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+              BottomNavigationBarItem(label: 'Timers', icon: Icon(Icons.timer)),
+              BottomNavigationBarItem(label: 'Trames', icon: Icon(Icons.timer)),
+              BottomNavigationBarItem(
+                  label: 'Groupes', icon: Icon(Icons.group)),
+              BottomNavigationBarItem(
+                  label: 'Compte', icon: Icon(Icons.verified_user))
+            ]));
   }
-
 
   Widget _button({required String title, VoidCallback? onPressed}) {
     return Expanded(
         child: ElevatedButton(
-          child: Text(
-            title,
-            style: const TextStyle(color: Colors.white),
-          ),
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.purple),
-          ),
-          onPressed: onPressed,
-        ));
+      child: Text(
+        title,
+        style: const TextStyle(color: Colors.white),
+      ),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.purple),
+      ),
+      onPressed: onPressed,
+    ));
   }
 }
-
-
-
