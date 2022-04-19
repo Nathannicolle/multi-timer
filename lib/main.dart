a14933f1082f5f5ec586482bd81ab894d3359b83
Nathannicolle <nathan-nicolle@orange.fr>
2022-04-20 00:45:28 +0200

    add message which evolve in depending on current hour

--- lib/main.dart
+++ lib/main.dart
@@ -37,104 +37,139 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final int _duration = 10;
   final CountDownController _controller = CountDownController();
+  final currentHour = DateTime.now().hour;
+
+  showHourMessage() {
+    if (currentHour >= 0 && currentHour <= 7) {
+      return const Text('Bonne nuit',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.grey, fontSize: 30));
+    } else if (currentHour <= 23 && currentHour >= 18) {
+      return const Text('Bonsoir',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.grey, fontSize: 30));
+    } else {
+      return const Text('Bonjour',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.grey, fontSize: 30));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize (
+        appBar: PreferredSize(
           preferredSize: const Size.fromHeight(80.0),
-          child: AppBar (
+          child: AppBar(
               backgroundColor: Colors.transparent,
               elevation: 0,
               shadowColor: Colors.grey,
               leading: CircleAvatar(
                 backgroundColor: Colors.transparent,
-                child: Image.asset("assets/img/logo_mini.png", width: 200.0, height: 200.0),
+                child: Image.asset("assets/img/logo_mini.png",
+                    width: 200.0, height: 200.0),
               ),
               actions: [
-                IconButton (
+                IconButton(
                   iconSize: 30,
                   icon: Icon(Icons.account_circle),
                   onPressed: () => {},
                 )
-              ]
-          ),
+              ]),
         ),
         backgroundColor: Color.fromRGBO(18, 18, 18, 1),
         body: Center(
-            child: CircularCountDownTimer(
-          // Countdown duration in Seconds.
-          duration: _duration,
+            child: Column(
+          children: <Widget>[
+            Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+              SizedBox(
+                height: 50,
+                child: Text("Accueil",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                        fontSize: 30)),
+              )
+            ]),
+            SizedBox(
+              height: 60,
+              child: showHourMessage(),
+            ),
+            CircularCountDownTimer(
+              // Countdown duration in Seconds.
+              duration: _duration,
 
-          // Countdown initial elapsed Duration in Seconds.
-          initialDuration: 0,
+              // Countdown initial elapsed Duration in Seconds.
+              initialDuration: 0,
 
-          // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
-          controller: _controller,
+              // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
+              controller: _controller,
 
-          // Width of the Countdown Widget.
-          width: MediaQuery.of(context).size.width / 2,
+              // Width of the Countdown Widget.
+              width: MediaQuery.of(context).size.width / 2,
 
-          // Height of the Countdown Widget.
-          height: MediaQuery.of(context).size.height / 2,
+              // Height of the Countdown Widget.
+              height: MediaQuery.of(context).size.height / 2,
 
-          // Ring Color for Countdown Widget.
-          ringColor: Colors.grey[300]!,
+              // Ring Color for Countdown Widget.
+              ringColor: Colors.grey[300]!,
 
-          // Ring Gradient for Countdown Widget.
-          ringGradient: null,
+              // Ring Gradient for Countdown Widget.
+              ringGradient: null,
 
-          // Filling Color for Countdown Widget.
-          fillColor: Colors.red[500]!,
+              // Filling Color for Countdown Widget.
+              fillColor: Colors.red[500]!,
 
-          // Filling Gradient for Countdown Widget.
-          fillGradient: null,
+              // Filling Gradient for Countdown Widget.
+              fillGradient: null,
 
-          // Background Color for Countdown Widget.
-          backgroundColor: Colors.blue[500],
+              // Background Color for Countdown Widget.
+              backgroundColor: Colors.blue[500],
 
-          // Background Gradient for Countdown Widget.
-          backgroundGradient: null,
+              // Background Gradient for Countdown Widget.
+              backgroundGradient: null,
 
-          // Border Thickness of the Countdown Ring.
-          strokeWidth: 20.0,
+              // Border Thickness of the Countdown Ring.
+              strokeWidth: 20.0,
 
-          // Begin and end contours with a flat edge and no extension.
-          strokeCap: StrokeCap.round,
+              // Begin and end contours with a flat edge and no extension.
+              strokeCap: StrokeCap.round,
 
-          // Text Style for Countdown Text.
-          textStyle: const TextStyle(
-            fontSize: 33.0,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
+              // Text Style for Countdown Text.
+              textStyle: const TextStyle(
+                fontSize: 33.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
 
-          // Format for the Countdown Text.
-          textFormat: CountdownTextFormat.S,
+              // Format for the Countdown Text.
+              textFormat: CountdownTextFormat.S,
 
-          // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
-          isReverse: false,
+              // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
+              isReverse: false,
 
-          // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
-          isReverseAnimation: false,
+              // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
+              isReverseAnimation: false,
 
-          // Handles visibility of the Countdown Text.
-          isTimerTextShown: true,
+              // Handles visibility of the Countdown Text.
+              isTimerTextShown: true,
 
-          // Handles the timer start.
-          autoStart: false,
+              // Handles the timer start.
+              autoStart: false,
 
-          // This Callback will execute when the Countdown Starts.
-          onStart: () {
-            // Here, do whatever you want
-            debugPrint('Countdown Started');
-          },
+              // This Callback will execute when the Countdown Starts.
+              onStart: () {
+                // Here, do whatever you want
+                debugPrint('Countdown Started');
+              },
 
-          // This Callback will execute when the Countdown Ends.
-          onComplete: () {
-            // Here, do whatever you want
-            debugPrint('Countdown Ended');
-          },
+              // This Callback will execute when the Countdown Ends.
+              onComplete: () {
+                // Here, do whatever you want
+                debugPrint('Countdown Ended');
+              },
+            )
+          ],
         )),
         floatingActionButton: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -165,10 +200,13 @@ class _MyHomePageState extends State<MyHomePage> {
             unselectedItemColor: Colors.grey,
             items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-              BottomNavigationBarItem(icon: Icon(Icons.timer), label:  'Timers'),
-              BottomNavigationBarItem(icon: Icon(Icons.pending_actions), label: 'Trames'),
-              BottomNavigationBarItem(icon: Icon(Icons.supervised_user_circle), label: 'Groupes'),
-              BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: 'Compte')
+              BottomNavigationBarItem(icon: Icon(Icons.timer), label: 'Timers'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.pending_actions), label: 'Trames'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.supervised_user_circle), label: 'Groupes'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle), label: 'Compte')
             ]));
   }
 
