95a64c64dea04be25a0de6c8c1353cf351302cb6
Nathannicolle <nathan-nicolle@orange.fr>
2022-05-15 23:16:16 +0200

    add build folder

---
+++ build/app/generated/source/buildConfig/release/fr/supavenir/multi_timer/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package fr.supavenir.multi_timer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "fr.supavenir.multi_timer";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
